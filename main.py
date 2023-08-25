482c2eba0e4032599332d6d2af84caa2277d0561
Black-Spades-Z <atos4ik@gmail.com>
2023-08-25 11:24:43 +0500

    Starting of the Group Chat

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+# def main():
+#     print("Hello World!")
+#
+# # Press the green button in the gutter to run the script.
+#
+#     main()
 
 
-# Press the green button in the gutter to run the script.
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return "Hello World!"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
