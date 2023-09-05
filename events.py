22ebad6f397789d3c3c9b7bb4fb57b147fcfcbb4
Black-Spades-Z <atos4ik@gmail.com>
2023-09-05 23:58:12 +0500

    Sharing username and userID is successfully finished

--- events.py
+++ events.py
@@ -1,7 +1,12 @@
+import json
+
 from extensions import socketio
 from datetime import datetime
 from flask import request
-from flask_socketio import emit
+from flask_socketio import emit, join_room, leave_room
+
+
+users = []
 
 @socketio.event
 def my_event(message):
@@ -13,6 +18,28 @@ def handle_connect():
 
 
 @socketio.on('user-data')
-def get_user(username):
+def get_user(data):
+    print('In get_user')
+    print(type(data))
+    username = data['username']
+    user_id = data['user_id']
+    users.append({
+         user_id : username,
+    })
     print(f'User,{username} has entered at {datetime.now()}')
-    
+
+@socketio.on('announce')
+def send_announce(announcement):
+    print(announcement)
+
+@socketio.on('join')
+def join_room(data):
+    room_name = data['room_name']
+    print(data['user_id'])
+    for user in users:
+        print(user)
+        print(user.keys)
+        if data['user_id'] == user.keys:
+            username = user[data['user_id']]
+    join_room(room_name)
+    emit('announce', f'{username} has entered to room : {room_name}')
